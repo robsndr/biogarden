39a8dbb1c5cdc620a1389b4606082e4e93105491
Robert Sander <robertsander22@yahoo.com>
2022-06-27 20:50:20 +0200

    Add linguistic complexity docs and example

--- src/analysis/seq.rs
+++ src/analysis/seq.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 
 use crate::ds::sequence::Sequence;
 use crate::error::{BioError, Result};
+use crate::algo::graph::suffix_tree::SuffixTreeBuilder;
 
 /// Gets a HashMap containing occurrences of nucleobases in a bio-sequence
 ///
@@ -17,8 +18,7 @@ use crate::error::{BioError, Result};
 /// use std::collections::HashMap;
 ///
 /// let dna = Sequence::from("AGCTTTTCATTCT");
-/// let counts = HashMap::<u8, usize>::from([(b'A', 2),(b'C', 3),
-///                                          (b'T', 7),(b'G', 1)]);
+/// let counts = HashMap::<u8, usize>::from([(b'A', 2), (b'C', 3), (b'T', 7), (b'G', 1)]);
 /// assert_eq!(count_nucleotides(&dna), counts);
 /// ```
 pub fn count_nucleotides(seq: &Sequence) -> HashMap<u8, usize> {
@@ -87,7 +87,7 @@ pub fn hamming_distance(seq1: &Sequence, seq2: &Sequence) -> Result<usize> {
 /// The Hamming distance provides a way to model point mutations transforming one genetic string into another.
 /// In practice, point mutations include insertions and deletions in addition to replacements only.
 /// This can produce genetic sequence that vary in length, and cannot be compared using hamming distance.
-/// In such scenarios, a measure of the minimum number of replacements / insertions / deletions between two sequences, is provided by edit distance.
+/// In such scenarios, a measure of the minimum number of replacements / insertions / deletions is provided by edit distance.
 ///
 /// # Arguments
 /// * `seq1`, `seq2` - sequences to between which the edit distance is calculated
@@ -132,7 +132,7 @@ pub fn edit_distance(seq1: &Sequence, seq2: &Sequence) -> Result<usize> {
 /// Calculate the ratio of transitions to transversions between two genetic strings
 ///   
 /// Point mutations between genetic strings occur in two flavors: transitions or transversions.
-/// Transitions occur for replacements between thymine and cytosine (A <-> C), 
+/// Transitions occur for replacements between thymine and cytosine (A <-> C),
 /// or adenine and guanine (uracil (U) for RNA) (A <-> G).
 /// The number of transitions to transversions is usually a bit higher for coding regions.
 /// This makes the transition-transversion ratio a suitable tool to identify those in a long strand.
@@ -144,54 +144,84 @@ pub fn edit_distance(seq1: &Sequence, seq2: &Sequence) -> Result<usize> {
 /// ```
 /// use biotech::analysis::seq::transition_transversion_ratio;
 /// use biotech::ds::sequence::Sequence;
-/// 
+///
 /// let a = Sequence::from("TTAGGGACTGGATTATTTCGTGATCGTTGTAGTTATTGGAAGTACGGGCATCAACCCAGTT");
 /// let b = Sequence::from("TCAACGGCTGGATAATTTCGCGATCGTGCTGGTTACTGGCGGTACGAGTGTTCCTTTGGGT");
 /// assert_eq!(transition_transversion_ratio(&a, &b).unwrap(), 1.875);
 /// ```
 pub fn transition_transversion_ratio(seq1: &Sequence, seq2: &Sequence) -> Result<f64> {
-
     if seq1.len() != seq2.len() {
         return Err(BioError::InvalidInputSize);
     }
-    
-    let missmatch_iter = seq1.into_iter().zip(seq2.into_iter()).filter(|(a, b)| a != b);
+
+    let missmatch_iter = seq1
+        .into_iter()
+        .zip(seq2.into_iter())
+        .filter(|(a, b)| a != b);
     let hamming_distance = missmatch_iter.clone().count() as f64;
-    let mut transitions : f64 = 0.0;
+    let mut transitions: f64 = 0.0;
     for (&a, &b) in missmatch_iter {
         transitions += match (a, b) {
-            (b'C', b'T') => { 1.0 }
-            (b'T', b'C') => { 1.0 }
-            (b'A', b'G') => { 1.0 }
-            (b'G', b'A') => { 1.0 }
-            _ => { 0.0 }
+            (b'C', b'T') => 1.0,
+            (b'T', b'C') => 1.0,
+            (b'A', b'G') => 1.0,
+            (b'G', b'A') => 1.0,
+            _ => 0.0,
         }
     }
     // transversions == hamming_distance - transitions
-    Ok(transitions / (hamming_distance  - transitions))
+    Ok(transitions / (hamming_distance - transitions))
 }
 
-
-
-
-pub fn linguistic_complexity(seq: Sequence) -> f32 {
-
+/// Returns the linguistic complexity of a genetic sequence
+///   
+/// The human genome contains a large number of repeats, with the ALU repeat being one of the most frequent.
+/// ALU repeats can create copies of themselves and reinsert those into the genome.
+/// This makes them occur up to a million times within the human genetic sequence.
+///
+/// The linguistic complexity provides a useful measure that helps quantify how repetitive a genome is.
+/// It is defined as the percentage of observed substrings of `s` to the total number that are theoretically possible.
+///
+/// # Arguments
+/// * `seq` - sequence for which linguistic complexity should be calculated
+///
+/// # Example
+/// ```
+/// use biotech::analysis::seq::linguistic_complexity;
+/// use biotech::ds::sequence::Sequence;
+///
+/// let a = Sequence::from("ATTTGGATT");
+/// assert_eq!(linguistic_complexity(&a).unwrap(), 0.875);
+/// ```
+pub fn linguistic_complexity(seq: &Sequence) -> Result<f32> {
     // Define alphabet
     let alphabet = HashSet::<u8>::from([b'A', b'C', b'T', b'G']);
     let alphabet_len = alphabet.len();
 
-    // Add the strings to tree and traverse from root to node. 
-    // Each root to node path will denote suffixes of a string. 
+    // Add the strings to tree and traverse from root to node.
+    // Each root to node path will denote suffixes of a string.
     let mut ukonnen_builder = SuffixTreeBuilder::new(alphabet);
-    let graph = ukonnen_builder.build(&seq);
+    let graph = ukonnen_builder.build(seq);
 
     // Count unique substrings
     // All the prefixes of these suffixes are unique substrings.
     // Their number can be obtained by summing-up the length of all edges
     let mut num_substrings = 0;
     for eid in graph.edges() {
-        let start = graph.get_edge(eid).data.as_ref().unwrap().suffix_start;
-        let stop = graph.get_edge(eid).data.as_ref().unwrap().suffix_stop;
+        // Get start edge
+        let start = graph
+            .get_edge(eid)
+            .data
+            .as_ref()
+            .ok_or(BioError::ItemNotFound)?
+            .suffix_start;
+        // Get stop edge
+        let stop = graph
+            .get_edge(eid)
+            .data
+            .as_ref()
+            .ok_or(BioError::ItemNotFound)?
+            .suffix_stop;
         num_substrings += stop - start + 1;
     }
 
@@ -199,19 +229,16 @@ pub fn linguistic_complexity(seq: Sequence) -> f32 {
     // limited by the number of substrings that can be formed from a given alphabet (4^k)
     // or by the number of k-windows that can be shifted within n-length string
     let mut max_complexity = 0;
-    for k in 1..seq.len()+1 {
+    for k in 1..seq.len() + 1 {
         // Perform 4^k only when 4^k < seq.len(), however 4^k test can result in overflow
-        // Use k < log4(seq.len()) instead. Convert log4(seq.len()) => log10(seq.len())/log10(4) 
-        // This will increase accuracy as log10 is better: 
+        // Use k < log4(seq.len()) instead. Convert log4(seq.len()) => log10(seq.len())/log10(4)
+        // This will increase accuracy as log10 is better:
         // Check -> https://doc.rust-lang.org/std/primitive.f64.html#method.log
         if (k as f32) < ((seq.len() as f32).log10() / (4.0_f32).log10()) {
             max_complexity += u128::pow(alphabet_len as u128, k as u32);
-        }
-        else {
+        } else {
             max_complexity += (seq.len() - k + 1) as u128;
         }
-
     }
-    
-    num_substrings as f32 / max_complexity as f32
+    Ok(num_substrings as f32 / max_complexity as f32)
 }
